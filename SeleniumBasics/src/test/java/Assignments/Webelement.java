5b1f13f35af22bb736e1f4ffbea5690638fc996d
Dell <neethujose120@gmail.com>
2022-05-17 14:13:25 +0530

    Committing webelement class

--- SeleniumBasics/src/test/java/Assignments/Webelement.java
+++ SeleniumBasics/src/test/java/Assignments/Webelement.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Webelement {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver",
 				System.getProperty("user.dir") + "\\BrowserDrivers\\Chromedriver\\chromedriver.exe");
@@ -27,47 +27,54 @@ public class Webelement {
 			System.out.println("Message Matching");
 
 		}
-		
+
 		driver.findElement(By.xpath("//input[@id='button-two']")).click();
-		
-		System.out.println(driver.findElement(By.xpath("//input[@id='check-box-one']")).isDisplayed());
-		System.out.println(driver.findElement(By.xpath("//input[@id='check-box-two']")).isDisplayed());
-		System.out.println(driver.findElement(By.xpath("//input[@id='check-box-three']")).isDisplayed());
-		System.out.println(driver.findElement(By.xpath("//input[@id='check-box-four']")).isDisplayed());
-		
-		System.out.println(driver.findElement(By.xpath("//input[@id='check-box-one']")).isSelected());
-		System.out.println(driver.findElement(By.xpath("//input[@id='check-box-two']")).isSelected());
-		System.out.println(driver.findElement(By.xpath("//input[@id='check-box-three']")).isSelected());
-		System.out.println(driver.findElement(By.xpath("//input[@id='check-box-four']")).isSelected());
-		
+
+		WebElement checkbox1 = driver.findElement(By.xpath("//input[@id='check-box-one']"));
+		WebElement checkbox2 = driver.findElement(By.xpath("//input[@id='check-box-two']"));
+		WebElement checkbox3 = driver.findElement(By.xpath("//input[@id='check-box-three']"));
+		WebElement checkbox4 = driver.findElement(By.xpath("//input[@id='check-box-four']"));
+
+		System.out.println(checkbox1.isDisplayed());
+		System.out.println(checkbox2.isDisplayed());
+		System.out.println(checkbox3.isDisplayed());
+		System.out.println(checkbox4.isDisplayed());
+
+		System.out.println(checkbox1.isSelected());
+		System.out.println(checkbox2.isSelected());
+		System.out.println(checkbox3.isSelected());
+		System.out.println(checkbox4.isSelected());
+
 		driver.navigate().refresh();
-		
-		WebElement checkbox1= driver.findElement(By.xpath("//input[@id='check-box-one']"));
-		WebElement checkbox2= driver.findElement(By.xpath("//input[@id='check-box-two']"));
-		WebElement checkbox3= driver.findElement(By.xpath("//input[@id='check-box-three']"));
-		WebElement checkbox4= driver.findElement(By.xpath("//input[@id='check-box-four']"));
-		
-		if(!checkbox1.isSelected()) {
+
+		checkbox1 = driver.findElement(By.xpath("//input[@id='check-box-one']"));
+		checkbox2 = driver.findElement(By.xpath("//input[@id='check-box-two']"));
+		checkbox3 = driver.findElement(By.xpath("//input[@id='check-box-three']"));
+		checkbox4 = driver.findElement(By.xpath("//input[@id='check-box-four']"));
+
+		Thread.sleep(3000);
+
+		if (!checkbox1.isSelected()) {
 			checkbox1.click();
 			System.out.println("checkbox 1 clicked");
 		}
-		
-		if(!checkbox2.isSelected()) {
+
+		if (!checkbox2.isSelected()) {
 			checkbox2.click();
 			System.out.println("checkbox 2 clicked");
 		}
-		
-		if(!checkbox3.isSelected()) {
+
+		if (!checkbox3.isSelected()) {
 			checkbox3.click();
 			System.out.println("checkbox 3 clicked");
 		}
-		
-		if(!checkbox4.isSelected()) {
+
+		if (!checkbox4.isSelected()) {
 			checkbox4.click();
 			System.out.println("checkbox 4 clicked");
 		}
-		
-		driver.close();
+
+		driver.close(); 
 	}
 
 }
